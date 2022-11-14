c724c5c8e862e62905073c8960bd25d8a5443358
Duke_Lee <847494628@qq.com>
2022-11-15 00:23:33 +0800

    update workflow

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import datetime
 import time
+import sys
 
 item = 3  # 0：羽毛球 1：乒乓球 2：游泳馆 3：健身房 4: 沙河健身房（测试）
 area_list = ['5982', '5983', '5984', '5985', '6002']
@@ -22,15 +23,21 @@ def get_cookies():
 
 
 if __name__ == "__main__":
+    if datetime.datetime.now().hour != 11:
+        sys.exit()
+
     timeIndexes = ["02", "03", "04", "05", "06", "07", "08"]
     chrome_options = Options()
     # 设置chrome浏览器无界面模式
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 15_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.29(0x18001d37) NetType/WIFI Language/zh_CN')
     browser = webdriver.Chrome(options=chrome_options)
 
-    for timeIndex in timeIndexes:
-        reserve_td_ids = room_list[item] + '_' + query_date + timeIndex
+    i = 0
+    while 1:
+        if i >= len(timeIndexes):
+            break
+        reserve_td_ids = room_list[item] + '_' + query_date + timeIndexes[i]
         req_url = 'https://reservation.bupt.edu.cn//index.php/Wechat/Booking/confirm_booking?area_id=' + area_id + '&td_id=' + reserve_td_ids + '&query_date=' + query_date
 
         # 开始请求
@@ -42,13 +49,17 @@ if __name__ == "__main__":
 
         # 再次请求
         browser.get(req_url)
+        if browser.title == "跳转中":  # 表示所选时间段还未开放
+            time.sleep(0.5)
+            continue
+        else:
+            i += 1
         # 选择余额支付
         browser.find_element(By.CSS_SELECTOR, "input[value='确认预约']").click()
-        time.sleep(0.3)
+        time.sleep(0.4)
         browser.find_element(By.CSS_SELECTOR, "input[type='radio'][id='package_pay_54168']").click()
         time.sleep(0.3)
         browser.find_element(By.CSS_SELECTOR, "button[class='btn btn-primary g-submit']").click()
-        # print(browser.page_source)
 
     #关闭浏览器
     browser.close()
